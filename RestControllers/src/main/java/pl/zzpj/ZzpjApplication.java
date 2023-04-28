3bf4251f3611e494c2e0bc5110b5e3e6636fd7f0
229872 Dygasiński Bartłomiej <229872@edu.p.lodz.pl>
2023-04-28 21:00:36 +0200

    First commit

---
+++ RestControllers/src/main/java/pl/zzpj/ZzpjApplication.java
@@ -0,0 +1,13 @@
+package pl.zzpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzpjApplication.class, args);
+    }
+
+}
