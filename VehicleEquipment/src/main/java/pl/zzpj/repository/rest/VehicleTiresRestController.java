3bc06cdead1abe266eaba17c61856dafc80f21ba
AverageCoffeeEnjoyer <bartlomiej.czajkowski20@gmail.com>
2023-06-18 11:00:09 +0200

    Poprawki checkstyle i walidacji UUID

--- VehicleEquipment/src/main/java/pl/zzpj/repository/rest/VehicleTiresRestController.java
+++ VehicleEquipment/src/main/java/pl/zzpj/repository/rest/VehicleTiresRestController.java
@@ -47,8 +47,8 @@ public class VehicleTiresRestController {
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Get all vehicle tires")
     @ApiResponse(responseCode = "200", description = "Returned list of tires",
-        content = {@Content(mediaType = "application/json",
-            schema = @Schema(implementation = VehicleTireOutputDto.class))})
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = VehicleTireOutputDto.class))})
     public List<VehicleTireOutputDto> getAll() {
         return tiresRestAdapter.getAllEquipment();
     }
@@ -57,135 +57,140 @@ public class VehicleTiresRestController {
     //    @RolesAllowed({GUEST})
     @Operation(summary = "Get vehicle tire by uuid")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Found the tire",
-            content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = VehicleTireOutputDto.class))}),
-        @ApiResponse(responseCode = "404", description = "Tire not found",
-            content = @Content)})
+            @ApiResponse(responseCode = "200", description = "Found the tire",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VehicleTireOutputDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Tire not found",
+                    content = @Content)})
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public VehicleTireOutputDto getEquipmentById(@PathVariable("id") UUID id)
-        throws VehicleEquipmentRestNotFoundException {
+    public VehicleTireOutputDto getEquipmentById(@Valid @PathVariable("id") UUID id)
+            throws VehicleEquipmentRestNotFoundException {
         return tiresRestAdapter.getEquipmentById(id);
     }
 
     @Operation(summary = "Create summer type tire")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "Summer type tire successfully created",
-            content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = VehicleTireOutputDto.class))}),
-        @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
-            content = @Content),
-        @ApiResponse(responseCode = "200", description = "Other error occurred",
-            content = @Content)
+            @ApiResponse(responseCode = "201", description = "Summer type tire successfully created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VehicleTireOutputDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
+                    content = @Content),
+            @ApiResponse(responseCode = "200", description = "Other error occurred",
+                    content = @Content)
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "summer", produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE)
-    public VehicleTireOutputDto createEquipmentSummer(@RequestBody VehicleTireInputCreateDto dto)
-        throws VehicleEquipmentRestCreateException, BadTireTypeException, VehicleEquipmentRestNotSpecifiedException {
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public VehicleTireOutputDto createEquipmentSummer(@Valid @RequestBody VehicleTireInputCreateDto dto)
+            throws VehicleEquipmentRestCreateException, BadTireTypeException,
+            VehicleEquipmentRestNotSpecifiedException {
         return tiresRestAdapter.addEquipment(dto, RestTireType.SUMMER);
     }
 
     @Operation(summary = "Create winter type tire")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "Winter type tire successfully created",
-            content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = VehicleTireOutputDto.class))}),
-        @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
-            content = @Content),
-        @ApiResponse(responseCode = "200", description = "Other error occurred",
-            content = @Content)
+            @ApiResponse(responseCode = "201", description = "Winter type tire successfully created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VehicleTireOutputDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
+                    content = @Content),
+            @ApiResponse(responseCode = "200", description = "Other error occurred",
+                    content = @Content)
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "winter", produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE)
-    public VehicleTireOutputDto createEquipmentWinter(@RequestBody VehicleTireInputCreateDto dto)
-        throws VehicleEquipmentRestCreateException, BadTireTypeException, VehicleEquipmentRestNotSpecifiedException {
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public VehicleTireOutputDto createEquipmentWinter(@Valid @RequestBody VehicleTireInputCreateDto dto)
+            throws VehicleEquipmentRestCreateException, BadTireTypeException,
+            VehicleEquipmentRestNotSpecifiedException {
         return tiresRestAdapter.addEquipment(dto, RestTireType.WINTER);
     }
 
     @Operation(summary = "Create special type tire")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "Special type tire successfully created",
-            content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = VehicleTireOutputDto.class))}),
-        @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
-            content = @Content),
-        @ApiResponse(responseCode = "200", description = "Other error occurred",
-            content = @Content)
+            @ApiResponse(responseCode = "201", description = "Special type tire successfully created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VehicleTireOutputDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
+                    content = @Content),
+            @ApiResponse(responseCode = "200", description = "Other error occurred",
+                    content = @Content)
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "special", produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE)
-    public VehicleTireOutputDto createEquipmentSpecial(@RequestBody VehicleTireInputCreateDto dto)
-        throws VehicleEquipmentRestCreateException, BadTireTypeException, VehicleEquipmentRestNotSpecifiedException {
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public VehicleTireOutputDto createEquipmentSpecial(@Valid @RequestBody VehicleTireInputCreateDto dto)
+            throws VehicleEquipmentRestCreateException, BadTireTypeException,
+            VehicleEquipmentRestNotSpecifiedException {
         return tiresRestAdapter.addEquipment(dto, RestTireType.SPECIAL);
     }
 
     @Operation(summary = "Create all season type tire")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "All season type tire successfully created",
-            content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = VehicleTireOutputDto.class))}),
-        @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
-            content = @Content),
-        @ApiResponse(responseCode = "200", description = "Other error occurred",
-            content = @Content)
+            @ApiResponse(responseCode = "201", description = "All season type tire successfully created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VehicleTireOutputDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
+                    content = @Content),
+            @ApiResponse(responseCode = "200", description = "Other error occurred",
+                    content = @Content)
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "all_season", produces = MediaType.APPLICATION_JSON_VALUE, consumes =
-        MediaType.APPLICATION_JSON_VALUE)
-    public VehicleTireOutputDto createEquipmentAll(@RequestBody @Valid VehicleTireInputCreateDto dto)
-        throws VehicleEquipmentRestCreateException, BadTireTypeException, VehicleEquipmentRestNotSpecifiedException {
+            MediaType.APPLICATION_JSON_VALUE)
+    public VehicleTireOutputDto createEquipmentAll(@Valid @RequestBody VehicleTireInputCreateDto dto)
+            throws VehicleEquipmentRestCreateException, BadTireTypeException,
+            VehicleEquipmentRestNotSpecifiedException {
         return tiresRestAdapter.addEquipment(dto, RestTireType.ALL_SEASON);
     }
 
     @Operation(summary = "Update tire by uuid")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "Tire successfully updated",
-            content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = VehicleTireOutputDto.class))}),
-        @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire", //todo fixme
-            content = @Content),
-        @ApiResponse(responseCode = "200", description = "Other error occurred",
-            content = @Content)
+            @ApiResponse(responseCode = "200", description = "Tire successfully updated",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VehicleTireOutputDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire", //todo fixme
+                    content = @Content),
+            @ApiResponse(responseCode = "200", description = "Other error occurred",
+                    content = @Content)
     })
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE)
-    public VehicleTireOutputDto updateEquipment(@PathVariable UUID id, @RequestBody VehicleTireInputUpdateDto dto)
-        throws VehicleEquipmentRestNotFoundException, VehicleEquipmentRestUpdateException,
-        VehicleEquipmentRestNotSpecifiedException {
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public VehicleTireOutputDto updateEquipment(@Valid @PathVariable UUID id,
+                                                @Valid @RequestBody VehicleTireInputUpdateDto dto)
+            throws VehicleEquipmentRestNotFoundException, VehicleEquipmentRestUpdateException,
+            VehicleEquipmentRestNotSpecifiedException {
         return tiresRestAdapter.updateEquipment(id, dto);
     }
 
     @Operation(summary = "Change archive status of a tire by uuid and boolean")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "204", description = "Archive status changed successfully",
-            content = {@Content(mediaType = "application/json",
-                schema = @Schema(implementation = VehicleTireOutputDto.class))}),
-        @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
-            content = @Content),
-        @ApiResponse(responseCode = "200", description = "Other error occurred",
-            content = @Content)
+            @ApiResponse(responseCode = "204", description = "Archive status changed successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VehicleTireOutputDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
+                    content = @Content),
+            @ApiResponse(responseCode = "200", description = "Other error occurred",
+                    content = @Content)
     })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping(path = "/{id}/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public VehicleTireOutputDto setArchiveStatusEquipment(@PathVariable UUID id, @PathVariable boolean status)
-        throws VehicleEquipmentRestUpdateException, VehicleEquipmentRestNotSpecifiedException,
-        VehicleEquipmentRestNotFoundException {
+    public VehicleTireOutputDto setArchiveStatusEquipment(@Valid @PathVariable UUID id, @PathVariable boolean status)
+            throws VehicleEquipmentRestUpdateException, VehicleEquipmentRestNotSpecifiedException,
+            VehicleEquipmentRestNotFoundException {
         return tiresRestAdapter.setArchiveStatusEquipment(id, status);
     }
 
     @Operation(summary = "Remove tire by uuid")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "Tire successfully removed",
-            content = @Content),
-        @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
-            content = @Content),
-        @ApiResponse(responseCode = "200", description = "Other error occurred",
-            content = @Content)
+            @ApiResponse(responseCode = "204", description = "Tire successfully removed",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Incorrect input for creating tire",
+                    content = @Content),
+            @ApiResponse(responseCode = "200", description = "Other error occurred",
+                    content = @Content)
     })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping(path = "/{id}")
